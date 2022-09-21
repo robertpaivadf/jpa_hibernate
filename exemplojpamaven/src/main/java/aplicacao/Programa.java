26035c0c69e6ceb6351b5778f74307c84bb044cf
Robert P Pereira <robertpaiva.df@gmail.com>
2022-09-21 20:20:21 -0300

    Versionamento inicial

---
+++ exemplojpamaven/src/main/java/aplicacao/Programa.java
@@ -0,0 +1,34 @@
+package aplicacao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import dominio.Pessoa;
+
+
+public class Programa {
+
+	public static void main(String[] args) {
+
+		Pessoa p1 = new Pessoa(null, "Carlos da Silva", "carlos@gmail.com");
+		Pessoa p2 = new Pessoa(null, "Joaqui Torres", "joaquim@gmail.com");
+		Pessoa p3 = new Pessoa(null, "Ana Maria", "ana@gmail.com");
+
+		// cria a conexão com o bc e toda o contexto de persistêrncia
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa"); // chama essa conf. lá do
+																							// persistence.xml
+		EntityManager em = emf.createEntityManager();
+
+		em.getTransaction().begin();
+		em.persist(p1);
+		em.persist(p2);
+		em.persist(p3);
+		
+		em.getTransaction().commit();
+		System.out.println("Pronto!");
+		
+		
+	}
+
+}
